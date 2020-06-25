7e2446686df18eafe6e20d93bbe7dc14a017e7ed
mozess <mozess2011@gmail.com>
2020-06-26 02:37:45 +0300

    Создал контроллер, который позволяет выводить список сотрудников компании (нужно вывести ФИО и возраст)

--- WebStoreTerehin/Controllers/HomeController.cs
+++ WebStoreTerehin/Controllers/HomeController.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStoreTerehin.Models;
 
 namespace WebStoreTerehin.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private static readonly List<Employee> _Employees = new List<Employee>
         {
-            return View();
+            new Employee
+            {
+                Id=1,
+                Surname="Иванов",
+                Name = "Иван",
+                Patronymic="Иванович",
+                Age=39,
+                DateOfEmployment=Convert.ToDateTime("01.01.2010")
+            },
+            new Employee
+            {
+                Id=2,
+                Surname="Петров",
+                Name = "Петр",
+                Patronymic="Петрович",
+                Age=49,
+                DateOfEmployment=Convert.ToDateTime("01.01.2015")
+            },
+            new Employee
+            {
+                Id=3,
+                Surname="Степанов",
+                Name = "Степан",
+                Patronymic="Степанович",
+                Age=18,
+                DateOfEmployment=Convert.ToDateTime("01.01.2020")
+            },
+        };
+        public IActionResult Index() => View();
+        public IActionResult Employees()
+        {
+            var employees  = _Employees;
+            return View(employees);
         }
     }
 }
