4db24a7b885a4698142ffdbd142f55950755a2c7
mozess <mozess2011@gmail.com>
2020-06-30 02:52:55 +0300

    Заготовки представлений для основных страниц

--- WebStoreTerehin/Controllers/HomeController.cs
+++ WebStoreTerehin/Controllers/HomeController.cs
@@ -9,5 +9,22 @@ namespace WebStoreTerehin.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
+
+        public IActionResult Blogs() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult Checkout() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
+
     }
 }
