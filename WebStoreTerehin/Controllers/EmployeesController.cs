b7320524ac0921c05e4fc227470c81869256ec78
mozess <mozess2011@gmail.com>
2020-06-30 07:14:27 +0300

    Добавление и удаление сотрудников

--- WebStoreTerehin/Controllers/EmployeesController.cs
+++ WebStoreTerehin/Controllers/EmployeesController.cs
@@ -40,6 +40,9 @@ namespace WebStoreTerehin.Controllers
         };
         public IActionResult Index()=> View(_Employees);
 
+        public IActionResult AddEmployees() => View();
+
+
         public IActionResult Details(int id)
         {
             var employee = _Employees.FirstOrDefault(e => e.Id == id);
@@ -47,5 +50,51 @@ namespace WebStoreTerehin.Controllers
                 return NotFound();
             return View(employee);
         }
+
+        public IActionResult DeleteEmployees(int id)
+        {
+            var employee = _Employees.FirstOrDefault(e => e.Id == id);
+            if (employee is null)
+                return NotFound();
+
+            _Employees.Remove(employee);
+
+            return RedirectToRoute("default", new { controller = "Employees", action = "Index"});
+        }
+        public IActionResult AddEmp(string surname, string name, string patronymic, string age, string employment)
+        {
+            if (surname is null)
+                return NotFound();
+            if (name is null)
+                return NotFound();
+            if (patronymic is null)
+                return NotFound();
+            int _age;
+            if (!int.TryParse(age, out _age))
+                return NotFound();
+
+            DateTime _employment;
+            if (!DateTime.TryParse(employment, out _employment))
+                return NotFound();
+
+            int id;
+            if (_Employees.Count() == 0)
+                id = 1;
+            else
+                id = _Employees.Max(e => e.Id) + 1;
+
+            _Employees.Add(new Employee
+            {
+                Id = id,
+                Surname = surname,
+                Name = name,
+                Patronymic = patronymic,
+                Age = _age,
+                DateOfEmployment = _employment
+            });
+
+            return RedirectToRoute("default", new { controller = "Employees", action = "Index" });
+        }
+
     }
 }
