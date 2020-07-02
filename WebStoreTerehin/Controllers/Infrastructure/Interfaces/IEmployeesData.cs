06a85816dbc31ebc8fc5a03e7f788109610e4d13
mozess <mozess2011@gmail.com>
2020-07-03 01:26:37 +0300

    Интерфейс сервиса работы с сотрудниками

---
+++ WebStoreTerehin/Controllers/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using WebStoreTerehin.Models;
+
+namespace WebStoreTerehin.Controllers.Infrastructure.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();
+
+        Employee GetById(int id);
+
+        int Add(Employee employee);
+
+        void Employee(Employee employee);
+
+        bool Delete(int id);
+
+        void SaveChanges();
+    }
+}
